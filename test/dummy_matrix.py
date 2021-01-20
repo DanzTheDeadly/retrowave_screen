56785a0ef4c9a95668ea5476b50c2eef04eb4088
DanzTheDeadly <danzerr@gmail.com>
2021-01-20 15:16:21 +0300

    Added matrix drawing in web

--- test/dummy_matrix.py
+++ test/dummy_matrix.py
@@ -2,14 +2,19 @@ from PIL import Image
 
 
 class RGBMatrixOptions:
-    def __init__(self, **kwargs):
-        self.options = kwargs
+    def __init__(self):
+        self.cols = None
+        self.rows = None
+        self.chain_length = None
+        self.parallel = None
+        self.gpio_slowdown = None
+        self.hardware_mapping = None
 
 
 class RGBMatrix:
     def __init__(self, options: RGBMatrixOptions):
         self.options = options
-        self.image = b'0'
+        self.image = bytearray(b'0' * self.options.cols * self.options.chain_length * self.options.rows * self.options.parallel)
         print('Matrix created')
 
     def SetImage(self, img):
@@ -20,4 +25,11 @@ class RGBMatrix:
         print('Set pixel at x={x} y={y} to {R}-{G}-{B}'.format(x=x, y=y, R=R, G=G, B=B))
 
     def get_image(self):
-        return self.image
+        img = ''
+        start = 0
+        for counter in range(self.options.cols * self.options.chain_length,
+                             len(self.image),
+                             self.options.cols * self.options.chain_length):
+            img += self.image[start:counter].decode() + '\n'
+            start = counter
+        return img.encode()
